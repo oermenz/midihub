771250885f4ee193a3166bd4024aa32049401e7c
oermenz <56572126+oermenz@users.noreply.github.com>
2024-10-27 19:24:28 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -53,6 +53,9 @@ sudo systemctl enable btmidi.service
 sudo systemctl start btmidi.service
 
 # Setup OLED screem
+sudo usermod -a -G i2c,spi,gpio oermens
+sudo apt install python3-dev python3-pip python3-numpy libfreetype6-dev libjpeg-dev build-essential
+sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev
 sudo apt install fonts-lato
 sudo cp midioled.py /usr/local/bin/
 sudo chmod a+x /usr/local/bin/midioled.py
