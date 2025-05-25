bd6856968c401be8d31540d9da06e68e42db6935
oermenz <56572126+oermenz@users.noreply.github.com>
2025-05-25 20:33:12 +0200

    Create midihub.rb

---
+++ midihub.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/ruby
+
+# unconnect everything
+system "aconnect -x"
+
+t = `aconnect -i -l`
+ports = []
+names = []
+t.lines.each do |l|
+  /client (\d*)\: '(.*)'/=~l
+  port = $1
+  name = $2
+  # we skip empty lines and the "Through" port
+  unless $1.nil? || $1 == '0' || /Through/=~l
+    ports << port
+    names << name
+  end
+end
+
+ports.each do |p1|
+  ports.each do |p2|
+    unless p1 == p2 # probably not a good idea to connect a port to itself
+      system  "aconnect #{p1}:0 #{p2}:0"
+    end
+  end
+end
+  
+cmd = "/usr/local/bin/lcd-show.py"
+if names.length>1 then
+  command = "#{cmd} #{names.map(&:inspect).join(' ')} "
+else
+  command = "#{cmd} '' 'No MIDI' 'connections' "
+end
+
+pid = spawn(command)
+Process.detach(pid)  
